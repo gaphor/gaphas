cfc048c28bcfcb74dc65f6759ad52d6d3a9f37d1
Arjan Molenaar <gaphor@gmail.com>
2006-06-07 07:49:55 +0000

    Added COPYING file.

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# Gaphas 
+# ======
+#
+# Gaphor's Canvas.
+#
+# This module contains the application independant parts of Gaphor's Canvas.
+# It can and may be used by others under the terms of the GNU LGPL licence.
+#
+
