43ad0e327b440829289102ecf2fc4e023c14e419
Arjan Molenaar <gaphor@gmail.com>
2006-07-17 09:05:35 +0000

    added revision and headurl

--- gaphas/__init__.py
+++ gaphas/__init__.py
@@ -1,11 +1,15 @@
-# Gaphas 
-# ======
-#
-# Gaphor's Canvas.
-#
-# This module contains the application independant parts of Gaphor's Canvas.
-# It can and may be used by others under the terms of the GNU LGPL licence.
-#
+"""
+Gaphas 
+======
+
+Gaphor's Canvas.
+
+This module contains the application independant parts of Gaphor's Canvas.
+It can and may be used by others under the terms of the GNU LGPL licence.
+"""
+
+__version__ = "$Revision$"
+# $HeadURL$
 
 from canvas import Canvas
 from item import Item, Line, Element, Handle
