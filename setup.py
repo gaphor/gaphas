fcf1a6bcd3db7281ff2b82db4086b1d68fc6980c
Arjan Molenaar <gaphor@gmail.com>
2006-12-05 07:58:21 +0000

    removed win32 trove cat.

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ It is the logical successor of the DiaCanvas library.
 
     classifiers=[
     'Development Status ::  4 - Beta',
-    'Environment :: Win32 (MS Windows)',
     'Environment :: X11 Applications :: GTK',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
