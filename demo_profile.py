426f3b7cdf1afa915c6efd79b234c8b3d3acd194
Arjan Molenaar <gaphor@gmail.com>
2007-05-23 08:41:34 +0000

    in gaphas' demo_profile.py: test if __main__ (otherwise it will confuse nose tests.

--- demo_profile.py
+++ demo_profile.py
@@ -1,11 +1,13 @@
 import demo
 
-import hotshot, hotshot.stats
-prof = hotshot.Profile('demo-gaphas.prof')
-prof.runcall(demo.main)
-prof.close()
-stats = hotshot.stats.load('demo-gaphas.prof')
-stats.strip_dirs()
-stats.sort_stats('time', 'calls')
-stats.print_stats(20)
+if __name__ == '__main__':
+    import hotshot, hotshot.stats
+    prof = hotshot.Profile('demo-gaphas.prof')
+    prof.runcall(demo.main)
+    prof.close()
+    stats = hotshot.stats.load('demo-gaphas.prof')
+    stats.strip_dirs()
+    stats.sort_stats('time', 'calls')
+    stats.print_stats(20)
 
+# vim:sw=4:et:ai
