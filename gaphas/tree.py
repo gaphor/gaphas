be437930306307d6fa18822b72fcba0cb5c79c91
Arjan Molenaar <gaphor@gmail.com>
2007-06-29 09:33:07 +0000

    Added a child->parent mapping to Tree. Also make reparent() move subtrees.

--- gaphas/tree.py
+++ gaphas/tree.py
@@ -25,25 +25,51 @@ class Tree(object):
         # Per entry a list of children is maintained.
         self._children = { None: [] }
 
+        # For easy and fast lookups, also maintain a child -> parent mapping
+        self._parents = { }
+
     nodes = property(lambda s: list(s._nodes))
 
     def get_parent(self, node):
         """
         Return the parent item of @node.
+
+        >>> tree = Tree()
+        >>> tree.add('n1')
+        >>> tree.add('n2', parent='n1')
+        >>> tree.get_parent('n2')
+        'n1'
         """
-        for item, children in self._children.items():
-            if node in children:
-                return item
+        return self._parents.get(node)
+#        for item, children in self._children.items():
+#            if node in children:
+#                return item
 
     def get_children(self, node):
         """
         Return all child objects of @node.
+
+        >>> tree = Tree()
+        >>> tree.add('n1')
+        >>> tree.add('n2', parent='n1')
+        >>> tree.add('n3', parent='n1')
+        >>> tree.get_children('n1')
+        ['n2', 'n3']
+        >>> tree.get_children('n2')
+        []
         """
         return self._children[node]
 
     def get_siblings(self, node):
         """
         Get all siblings of @node, including @node.
+
+        >>> tree = Tree()
+        >>> tree.add('n1')
+        >>> tree.add('n2', parent='n1')
+        >>> tree.add('n3', parent='n1')
+        >>> tree.get_siblings('n2')
+        ['n2', 'n3']
         """
         parent = self.get_parent(node)
         return self._children[parent]
@@ -51,6 +77,17 @@ class Tree(object):
     def get_next_sibling(self, node):
         """
         Return the node on the same level after @node.
+
+        >>> tree = Tree()
+        >>> tree.add('n1')
+        >>> tree.add('n2', parent='n1')
+        >>> tree.add('n3', parent='n1')
+        >>> tree.get_next_sibling('n2')
+        'n3'
+        >>> tree.get_next_sibling('n3') # doctest: +ELLIPSIS
+        Traceback (most recent call last):
+            ...
+        IndexError: list index out of range
         """
         parent = self.get_parent(node)
         siblings = self._children[parent]
@@ -59,6 +96,17 @@ class Tree(object):
     def get_previous_sibling(self, node):
         """
         Return the node on the same level before @node.
+
+        >>> tree = Tree()
+        >>> tree.add('n1')
+        >>> tree.add('n2', parent='n1')
+        >>> tree.add('n3', parent='n1')
+        >>> tree.get_previous_sibling('n3')
+        'n2'
+        >>> tree.get_previous_sibling('n2') # doctest: +ELLIPSIS
+        Traceback (most recent call last):
+            ...
+        IndexError: list index out of range
         """
         parent = self.get_parent(node)
         siblings = self._children[parent]
@@ -70,6 +118,17 @@ class Tree(object):
     def get_all_children(self, node):
         """
         Iterate all children (and children of children and so forth)
+
+        >>> tree = Tree()
+        >>> tree.add('n1')
+        >>> tree.add('n2', parent='n1')
+        >>> tree.add('n3', parent='n2')
+        >>> tree.get_children('n1')
+        ['n2']
+        >>> tree.get_all_children('n1') # doctest: +ELLIPSIS
+        <generator object at 0x...>
+        >>> list(tree.get_all_children('n1'))
+        ['n2', 'n3']
         """
         children = self.get_children(node)
         for c in children:
@@ -80,6 +139,19 @@ class Tree(object):
     def get_ancestors(self, node):
         """
         Iterate all parents and parents of parents, etc.
+
+        >>> tree = Tree()
+        >>> tree.add('n1')
+        >>> tree.add('n2', parent='n1')
+        >>> tree.add('n3', parent='n2')
+        >>> tree.get_parent('n3')
+        'n2'
+        >>> tree.get_ancestors('n3') # doctest: +ELLIPSIS
+        <generator object at 0x...>
+        >>> list(tree.get_ancestors('n3'))
+        ['n2', 'n1']
+        >>> list(tree.get_ancestors('n1'))
+        []
         """
         parent = self.get_parent(node)
         while parent:
@@ -109,6 +181,8 @@ class Tree(object):
         """
         Add @node to the tree. @parent is the parent node, which may
         be None if the item should be added to the root item.
+
+        For usage, see the unit tests.
         """
         assert not self._children.get(node)
         siblings = self._children[parent]
@@ -116,35 +190,83 @@ class Tree(object):
         siblings.append(node)
         # Create new entry for it's own children:
         self._children[node] = []
+        if parent:
+            self._parents[node] = parent
 
     @observed
     def remove(self, node):
         """
         Remove @node from the tree.
+
+        For usage, see the unit tests.
         """
         # First remove children:
-        children = list(self._children[node])
-        for c in children:
+        for c in list(self._children[node]):
             self.remove(c)
         # Remove from parent item
         self.get_siblings(node).remove(node)
         # Remove data entries:
         del self._children[node]
         self._nodes.remove(node)
+        try:
+            del self._parents[node]
+        except KeyError:
+            pass
 
 
+    def _reparent_nodes(self, node, parent):
+        """
+        Helper for reparent().
+        The _children and _parent trees can be left intact as far as children
+        of the reparented node are concerned. Only the position in the
+        _nodes list changes.
+        """
+        self._nodes.remove(node)
+        self._add_to_nodes(node, parent)
+        for c in self._children[node]:
+            self._reparent_nodes(c, node)
+        
     @observed
     def reparent(self, node, parent):
         """
-        Set new parent for a node. Parent can be None.
-        """
-        # remove from parent
-        self.get_siblings(node).remove(node)
+        Set new parent for a node. Parent can be None, indicating it's added
+        to the top.
 
-        # add to new parent
-        siblings = self._children[parent]
-        self._add_to_nodes(node, parent)
-        siblings.append(node)
+        >>> tree = Tree()
+        >>> tree.add('n1')
+        >>> tree.add('n2', parent='n1')
+        >>> tree.add('n3', parent='n1')
+        >>> tree.nodes
+        ['n1', 'n2', 'n3']
+        >>> tree.reparent('n2', 'n3')
+        >>> tree.get_parent('n2')
+        'n3'
+        >>> tree.get_children('n3')
+        ['n2']
+        >>> tree.nodes
+        ['n1', 'n3', 'n2']
+
+        If a node contains children, those are also moved::
+        
+        >>> tree.add('n4')
+        >>> tree.nodes
+        ['n1', 'n3', 'n2', 'n4']
+        >>> tree.reparent('n1', 'n4')
+        >>> tree.get_parent('n1')
+        'n4'
+        >>> list(tree.get_all_children('n4'))
+        ['n1', 'n3', 'n2']
+        >>> tree.nodes
+        ['n4', 'n1', 'n3', 'n2']
+        """
+        # Add to new parent's children:
+        self.get_siblings(node).remove(node)
+        self._children[parent].append(node)
+        self._parents[node] = parent
+        
+        # reorganize nodes
+        self._reparent_nodes(node, parent)
+
 
     reversible_pair(add, remove,
                     bind1={'parent': lambda self, node: self.get_parent(node) })
@@ -153,5 +275,16 @@ class Tree(object):
     disable_dispatching(add)
     disable_dispatching(remove)
 
+
+__test__ = {
+    'Tree.add': Tree.add,
+    'Tree.remove': Tree.remove,
+    'Tree.reparent': Tree.reparent,
+    }
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
 # vi:sw=4:et
