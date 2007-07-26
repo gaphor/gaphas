651054d0fc1b31297edc03050b3a6869dc34f36f
Arjan Molenaar <gaphor@gmail.com>
2007-07-26 12:44:23 +0000

    fixed rubberband tool: now selection can be done in all directions.

--- gaphas/tool.py
+++ gaphas/tool.py
@@ -510,40 +510,40 @@ class HandleTool(Tool):
 class RubberbandTool(Tool):
 
     def __init__(self):
-        self.rect = Rectangle()
+        self.x0, self.y0, self.x1, self.y1 = 0, 0, 0, 0
 
     def on_button_press(self, context, event):
         context.grab()
-        self.rect.x, self.rect.y = event.x, event.y
-        self.rect.x1, self.rect.y1 = event.x, event.y
+        self.x0, self.y0 = event.x, event.y
+        self.x1, self.y1 = event.x, event.y
         return True
 
     def on_button_release(self, context, event):
         context.ungrab()
         self.queue_draw(context.view)
-        r = self.rect
-        context.view.select_in_rectangle(r)
+        x0, y0, x1, y1 = self.x0, self.y0, self.x1, self.y1
+        context.view.select_in_rectangle((min(x0, x1), min(y0, y1),
+                 abs(x1 - x0), abs(y1 - y0)))
         return True
 
     def on_motion_notify(self, context, event):
         if event.state & gtk.gdk.BUTTON_PRESS_MASK:
             view = context.view
             self.queue_draw(view)
-            # TODO: keep track of what's expanding (top/bottom, left/right)
-            self.rect.x1, self.rect.y1 = event.x, event.y
+            self.x1, self.y1 = event.x, event.y
             self.queue_draw(view)
             return True
 
     def queue_draw(self, view):
-        r = self.rect
-        view.queue_draw_area(*r)
+        x0, y0, x1, y1 = self.x0, self.y0, self.x1, self.y1
+        view.queue_draw_area(min(x0, x1), min(y0, y1), abs(x1 - x0), abs(y1 - y0))
 
     def draw(self, context):
         cr = context.cairo
-        r = self.rect
+        x0, y0, x1, y1 = self.x0, self.y0, self.x1, self.y1
         cr.set_line_width(1.0)
         cr.set_source_rgba(.5, .5, .7, .6)
-        cr.rectangle(*r)
+        cr.rectangle(min(x0, x1), min(y0, y1), abs(x1 - x0), abs(y1 - y0))
         cr.fill()
 
 
