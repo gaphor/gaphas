a2a76fb13aa6d2fd0eef830753cb77c0ec871e71
Arjan Molenaar <gaphor@gmail.com>
2008-07-16 09:45:17 +0000

    more profile cleanup.

--- demo_profile.py
+++ demo_profile.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
 
-import pygtk
-pygtk.require('2.0') 
-
-import sys
-import math
-import gtk
-from gaphas import Canvas, GtkView, View
-from gaphas.examples import Box, Text, DefaultExampleTool
-from gaphas.item import Line, NW, SE
-from gaphas.tool import PlacementTool, HandleTool
-from gaphas.painter import ItemPainter
-from gaphas import state
-from gaphas.util import text_extents
-
 from demo import *
 
 
