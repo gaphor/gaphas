6f6d9f07d7d7b7ab93402a41bb915e0f7366b2c1
Dan Yeaw <dan@yeaw.me>
2018-12-25 13:20:10 -0500

    Revert "Remove extra doc folder"
    This reverts commit fa8eaa13
    
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

---
+++ doc/conf.py
@@ -0,0 +1,8 @@
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest", "sphinx.ext.todo"]
+project = "Gaphas"
+version = "0.5"
+release = version
+source_suffix = ".txt"
+master_doc = "contents"
+# html_theme = 'traditional'
+unused_docs = "constraints.txt index.txt comparison.txt"
