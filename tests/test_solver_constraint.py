976102b3628445be9aedbb5af07e2020822ae805
Arjan Molenaar <gaphor@gmail.com>
2020-10-18 20:40:35 +0200

    Add test for multi-constraint

--- tests/test_solver_constraint.py
+++ tests/test_solver_constraint.py
@@ -1,32 +1,46 @@
-from gaphas.solver import Constraint, Projection, Variable
+import pytest
+
+from gaphas.solver import Constraint, MultiConstraint, Projection, Variable
 
 
-def test_constraint_propagates_variable_changed():
-    v = Variable()
-    c = Constraint(v)
+@pytest.fixture
+def handler():
     events = []
 
-    def handler(c):
-        events.append(c)
+    def handler(e):
+        events.append(e)
 
+    handler.events = events  # type: ignore[attr-defined]
+    return handler
+
+
+def test_constraint_propagates_variable_changed(handler):
+    v = Variable()
+    c = Constraint(v)
     c.add_handler(handler)
 
     v.value = 3
 
-    assert events == [c]
+    assert handler.events == [c]
 
 
-def test_constraint_propagates_variable_wrapped_in_projection():
+def test_constraint_propagates_variable_wrapped_in_projection(handler):
     v = Variable()
     p = Projection(v)
     c = Constraint(p)
-    events = []
-
-    def handler(c):
-        events.append(c)
-
     c.add_handler(handler)
 
     v.value = 3
 
-    assert events == [c]
+    assert handler.events == [c]
+
+
+def test_multi_constraint(handler):
+    v = Variable()
+    c = Constraint(v)
+    m = MultiConstraint(c)
+    m.add_handler(handler)
+
+    v.value = 3
+
+    assert handler.events == [c]
