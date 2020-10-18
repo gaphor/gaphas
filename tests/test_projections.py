4a4494615e13bb6ed4b6782e49a851655b622da3
Arjan Molenaar <gaphor@gmail.com>
2020-10-18 16:01:26 +0200

    Model MatrixProjection as a constraint
    This looks really neat, since now the constraint can be registered with
    the solver and we do not have to manage it separately.

--- tests/test_projections.py
+++ tests/test_projections.py
@@ -3,7 +3,12 @@ import pytest
 from gaphas.connector import Position
 from gaphas.matrix import Matrix
 from gaphas.projections import MatrixProjection
-from gaphas.solver import Variable
+from gaphas.solver import Solver, Variable
+
+
+@pytest.fixture
+def solver():
+    return Solver()
 
 
 def test_matrix_projection_exposes_variables():
@@ -16,15 +21,18 @@ def test_matrix_projection_exposes_variables():
 @pytest.mark.parametrize(
     "position,matrix,result",
     [
-        [(0, 0), (1, 0, 0, 1, 0, 0), (0, 0)],
-        [(2, 4), (2, 0, 0, 1, 2, 3), (6, 7)],
-        [(2, 4), (2, 0, 0, 1, 2, 3), (6, 7)],
+        [(0, 0), Matrix(1, 0, 0, 1, 0, 0), (0, 0)],
+        [(2, 4), Matrix(2, 0, 0, 1, 2, 3), (6, 7)],
+        [(2, 4), Matrix(2, 0, 0, 1, 2, 3), (6, 7)],
     ],
 )
-def test_position_is_projected(position, matrix, result):
-    pos = Position(position)
-    m = Matrix(*matrix)
-    proj = MatrixProjection(pos, m)
+def test_position_is_projected(solver, position, matrix, result):
+    pos = Position((0, 0))
+    proj = MatrixProjection(pos, matrix)
+    solver.add_constraint(proj)
+
+    pos.x, pos.y = position
+    solver.solve()
 
     assert proj.x == result[0]
     assert proj.y == result[1]
@@ -33,17 +41,23 @@ def test_position_is_projected(position, matrix, result):
 @pytest.mark.parametrize(
     "position,matrix,result",
     [
-        [(2, 0), (1, 0, 0, 1, 0, 0), (2, 0)],
-        [(2, 0), (1, 0, 0, 1, 4, 3), (-2, -3)],
-        [(1, 2), (1, 0, 0, 1, 4, 3), (-3, -1)],
+        [(0, 0), Matrix(1, 0, 0, 1, 0, 0), (0, 0)],
+        [(2, 0), Matrix(1, 0, 0, 1, 0, 0), (2, 0)],
+        [(2, 0), Matrix(1, 0, 0, 1, 4, 3), (-2, -3)],
+        [(1, 2), Matrix(1, 0, 0, 1, 4, 3), (-3, -1)],
     ],
 )
-def test_projection_updates_original(position, matrix, result):
+def test_projection_updates_original(solver, position, matrix, result):
     pos = Position((0, 0))
-    m = Matrix(*matrix)
 
-    proj = MatrixProjection(pos, m)
+    proj = MatrixProjection(pos, matrix)
+    solver.add_constraint(proj)
+
     proj.x, proj.y = position
 
+    solver.solve()
+
+    print(pos)
+    print(proj.x, proj.y)
     assert pos.x == result[0]
     assert pos.y == result[1]
