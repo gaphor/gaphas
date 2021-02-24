e56d21b8c8c3b154d5e594be74e121cda95c43c3
Arjan Molenaar <gaphor@gmail.com>
2021-02-24 22:27:31 +0100

    Keep GTK 3 the default

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,7 @@ import os
 
 import gi
 
-TEST_GTK_VERSION = os.getenv("TEST_GTK_VERSION", "4.0")
+TEST_GTK_VERSION = os.getenv("TEST_GTK_VERSION", "3.0")
 
 gi.require_version("Gdk", TEST_GTK_VERSION)
 gi.require_version("Gtk", TEST_GTK_VERSION)
