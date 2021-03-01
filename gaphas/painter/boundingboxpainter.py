3fd2733e0a2f1eb724c947ad5e0704367c45154a
Arjan Molenaar <gaphor@gmail.com>
2021-03-01 09:00:03 +0100

    Clean up bounding box painter

--- gaphas/painter/boundingboxpainter.py
+++ gaphas/painter/boundingboxpainter.py
@@ -96,16 +96,14 @@ class BoundingBoxPainter:
     """This specific case of an ItemPainter is used to calculate the bounding
     boxes (in cairo device coordinates) for the items."""
 
-    draw_all = True
-
     def __init__(
         self,
         item_painter: ItemPainterType,
     ):
         self.item_painter = item_painter
 
-    def paint_item(self, item: Item, cairo: CairoContext) -> Rectangle:
-        bbctx = CairoBoundingBoxContext(cairo)
+    def paint_item(self, item: Item, cr: CairoContext) -> Rectangle:
+        bbctx = CairoBoundingBoxContext(cr)
         self.item_painter.paint_item(item, bbctx)
         # Bounding box is in view (cairo root) coordinates
         bounds = bbctx.get_bounds()
@@ -113,22 +111,20 @@ class BoundingBoxPainter:
         # Update bounding box with handles.
         i2c = item.matrix_i2c.transform_point
         for h in item.handles():
-            cx, cy = cairo.user_to_device(*i2c(*h.pos))
+            cx, cy = cr.user_to_device(*i2c(*h.pos))
             bounds += (cx - 5, cy - 5, 9, 9)
 
         bounds.expand(1)
         return bounds
 
-    def paint(
-        self, items: Collection[Item], cairo: CairoContext
-    ) -> Dict[Item, Rectangle]:
+    def paint(self, items: Collection[Item], cr: CairoContext) -> Dict[Item, Rectangle]:
         """Draw the items, return the bounding boxes (in cairo device
         coordinates)."""
         paint_item = self.paint_item
-        boxes: Dict[Item, Rectangle] = {item: paint_item(item, cairo) for item in items}
+        boxes: Dict[Item, Rectangle] = {item: paint_item(item, cr) for item in items}
         return boxes
 
-    def bounding_box(self, items: Collection[Item], cairo: CairoContext) -> Rectangle:
+    def bounding_box(self, items: Collection[Item], cr: CairoContext) -> Rectangle:
         """Get the unified bounding box of the rendered items."""
-        boxes = self.paint(items, cairo)
+        boxes = self.paint(items, cr)
         return functools.reduce(operator.add, boxes.values())
