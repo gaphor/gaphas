2426dce25ac0466f9f1db38a2357fbbecf1fd7b7
Dan Yeaw <dan@yeaw.me>
2023-06-06 22:05:33 -0400

    Always build and upload sdist and wheel

---
+++ .github/scripts/metadata.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "GITHUB_REF is $GITHUB_REF"
+TAG="${GITHUB_REF/refs\/tags\//}"
+echo "TAG is $TAG"
+if ! [ -x "$(command -v poetry)" ]; then
+    echo 'Poetry not found!' >&2
+    exit 1
+fi
+VERSION="$(poetry version --no-ansi | cut -d' ' -f2)"
+echo "VERSION is $VERSION"
+
+if [[ "$GITHUB_REF" =~ refs\/tags\/.* && "$TAG" == "${VERSION}" ]]
+then
+    REV=""
+    RELEASE="true"
+else
+    # PEP440 version scheme, different from semver 2.0
+    REV=".dev${GITHUB_RUN_NUMBER:-0}+${GITHUB_SHA:0:8}"
+    RELEASE="false"
+
+    # Update version, so it will also show in the Gaphor application
+    poetry version "${VERSION}""${REV}"
+fi
+
+echo "version=${VERSION}${REV}" >> "$GITHUB_OUTPUT"
+echo "release=${RELEASE}" >> "$GITHUB_OUTPUT"
